a89f33cf738d8cfea3d3d9a856de8c1622dc7de2
Rayan Ribeiro <rayanrib2012@hotmail.com>
2025-01-03 08:25:29 -0300

    Added main website menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
 export default function PHome() {
   return (
     <main>
-      <h1>teste pagina</h1>
+      <div
+        style={{
+          width: "100%",
+          height: 600,
+          backgroundColor: "#121212",
+          marginBottom: 24,
+        }}
+      >
+        <h1>teste</h1>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: 600,
+          backgroundColor: "#121212",
+          marginBottom: 24,
+        }}
+      >
+        <h1>teste</h1>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: 600,
+          backgroundColor: "#121212",
+          marginBottom: 24,
+        }}
+      >
+        <h1>teste</h1>
+      </div>
     </main>
   );
 }
--- src/components/headerDefault/CHeaderDefault.tsx
+++ src/components/headerDefault/CHeaderDefault.tsx
@@ -1,7 +1,36 @@
+"use client";
+import Link from "next/link";
+import styles from "./style.module.scss";
+import { useEffect, useState } from "react";
+
 export default function CHeaderDefault() {
+  const [top, setTop] = useState(true);
+
+  function scrollHandler() {
+    window.scrollY > 0 ? setTop(false) : setTop(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, [top]);
+
   return (
-    <header>
-      <h2>HEADER</h2>
+    <header className={`${styles.header} ${!top && styles.fixed}`}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href="/">Dev Motors</Link>
+          </div>
+          <nav className={styles.nav}>
+            <Link href="/">HOME</Link>
+            <Link href="/#servicos">SERVIÇOS</Link>
+            <Link href="/#contatos">CONTATOS</Link>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
