6409fe26b36404f09c19959208d0158f389cf976
Rayan Ribeiro <rayanrib2012@hotmail.com>
2025-01-07 10:57:46 -0300

    Added styling to the sub menu component

--- src/components/home/subMenu/CHomeSubMenu.tsx
+++ src/components/home/subMenu/CHomeSubMenu.tsx
@@ -1,17 +1,42 @@
+"use client";
 import Link from "next/link";
 import styles from "./styles.module.scss";
-import { Menu } from "lucide-react";
+import { X, Menu } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function CHomeSubMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    function handleRessize() {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    }
+    window.addEventListener("resize", handleRessize);
+    return () => window.removeEventListener("resize", handleRessize);
+  }, []);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <section className={styles.submenu}>
-      <div className={styles.submenuIcon}>
+      <div className={styles.submenuIcon} onClick={toggleMenu}>
         <Menu size={34} color="#121212" />
-        Menu
+        Serviços
       </div>
-      <ul className={styles.ul}>
+      <ul className={`${styles.ul} ${isOpen && styles.open}`}>
+        {isOpen && (
+          <button className={styles.closeSubmenu}>
+            <X size={34} color="#121212" onClick={toggleMenu} />
+          </button>
+        )}
         <li>
           <Link href="/post/pagina-1">Página 1</Link>
+        </li>
+        <li>
           <Link href="/post/pagina-2">Página 2</Link>
         </li>
       </ul>
