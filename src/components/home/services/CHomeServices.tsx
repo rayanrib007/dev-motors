1af846a94d8b3adb79f745aa84c6087cb471a2ea
rayan ribeiro <rayanrib2012@hotmail.com>
2025-02-17 20:18:24 -0300

    Added style to the services component

--- src/components/home/services/CHomeServices.tsx
+++ src/components/home/services/CHomeServices.tsx
@@ -8,20 +8,41 @@ export default function CHomeServices({
   homeData: IHomeDataProtocols;
 }) {
   return (
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h1 className={styles.title}>Sobre</h1>
-        <p>{homeData.object.metadata.about.description}</p>
-      </article>
-      <div className={styles.bannerAbout}>
-        <Image
-          className={styles.imageAbout}
-          alt="Imagem ilustrativa sobre a empresa"
-          quality={100}
-          fill={true}
-          src={homeData.object.metadata.about.banner.url}
-        />
-      </div>
-    </section>
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{homeData.object.metadata.about.description}</p>
+        </article>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagem ilustrativa sobre a empresa"
+            quality={100}
+            fill={true}
+            src={homeData.object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+      <section className={styles.services}>
+        {homeData.object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.service}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt="Imagem do serviço"
+                quality={100}
+                fill={true}
+                src={service.image.url}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
