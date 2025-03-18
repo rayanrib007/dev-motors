e301a08e6a01b877241ac1bbde5c6213afdceb7a
rayan ribeiro <rayanrib2012@hotmail.com>
2025-03-17 22:14:49 -0300

    Added dynamic page styling and rendering

--- src/container/page/CTNViewPage.tsx
+++ src/container/page/CTNViewPage.tsx
@@ -1,10 +1,50 @@
+import CHero from "@/components/general/hero/CHero";
 import { IPagesDataProtocol } from "@/interfaces/IPages";
+import { Phone } from "lucide-react";
+import CTNCenterElementOnPage from "../general/CTNCenterElementOnPage";
+import styles from "./styles.module.scss";
+import Image from "next/image";
 
 export default function CTNViewPage({
   dataPage,
 }: {
   dataPage: IPagesDataProtocol;
 }) {
-  console.log(dataPage);
-  return <div>CTNViewPage</div>;
+  return (
+    <>
+      <CHero
+        heading={dataPage.title}
+        bannerUrl={dataPage.metadata.banner.url}
+        buttonTitle={dataPage.metadata.button.title}
+        buttonUrl={dataPage.metadata.button.url}
+        icon={<Phone size={24} color="#fff" />}
+      />
+      <CTNCenterElementOnPage>
+        <section className={styles.about}>
+          <article className={styles.innerAbout}>
+            <h1>{dataPage.metadata.description.title}</h1>
+            <p>{dataPage.metadata.description.text}</p>
+            {dataPage.metadata.description.button_active && (
+              <a
+                href={dataPage.metadata.description.button_url}
+                target="_blank"
+                className={styles.link}
+              >
+                {dataPage.metadata.description.button_title}
+              </a>
+            )}
+          </article>
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              alt={dataPage.title}
+              quality={100}
+              fill={true}
+              src={dataPage.metadata.description.banner.url}
+            />
+          </div>
+        </section>
+      </CTNCenterElementOnPage>
+    </>
+  );
 }
