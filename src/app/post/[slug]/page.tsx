de29d8f099cfb57e4e6900382757ba75ec110019
rayan ribeiro <rayanrib2012@hotmail.com>
2025-03-25 21:39:21 -0300

    Added dynamic metadata to the project

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,5 +1,47 @@
 import { getDataPage } from "@/api/APages";
 import CTNViewPage from "@/container/page/CTNViewPage";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  try {
+    const { slug } = await params;
+    const dataPage = await getDataPage(slug);
+    return {
+      title: `DevMotors - ${dataPage.data.objects[0].title}`,
+      description: dataPage.data.objects[0].metadata.description.text,
+      keywords: [
+        "oficina",
+        "oficina de carros",
+        "carros",
+        "troca de óleos",
+        "manutenção de carros",
+      ],
+      openGraph: {
+        title: "DevMotors - Sua oficina personalizada!",
+        images: [`${dataPage.data.objects[0].metadata.banner.url}`],
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
+        },
+      },
+    };
+  } catch (error) {
+    return {
+      title: "DevMotors - Sua oficina personalizada!",
+      description: "Oficiona de carros em São Paulo",
+    };
+  }
+}
 
 export default async function PPost({
   params,
